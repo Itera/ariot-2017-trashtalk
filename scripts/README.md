272edfc5f5e1cbac4a5462f57bb68024e659d7fc
Håkon Dissen <hakon.dissen@itera.no>
2017-03-10 22:33:14 +0100

    feat: add a lot of data here!

---
+++ scripts/README.md
@@ -0,0 +1,8 @@
+SCRIPTS
+=======
+
+#generate_dummy_data.py
+
+Generates nice sensor data, mathces it with postal numbers from Ullern, Oslo and stores it in zhe cloud.
+
+Dont be evil, don run this during the competition :)
--- scripts/generate_dummy_data.py
+++ scripts/generate_dummy_data.py
@@ -1,7 +1,8 @@
 import json
 import random
-from collections import defaultdict
 from functools import partial
+import requests
+
 
 def generate_accelorometer_point():
     return random.uniform(-1.5, 1.5)
@@ -56,5 +57,57 @@ def generate_n_per_guid(guids, n_func=partial(random.randrange, 1, 10)):
     return {guid: [generate_one_random_point() for n in range(n_func())] for guid in guids}
 
 
+def load_addresses():
+    return requests.get('http://www.aaberge.net/stuff/addresses_oslo.json').json()['adresser']
+
+
+def get_subset_of_addresses(filters=(lambda x: x['postnr'].lower() in ['283', '378', '379', '281'],), **kwargs):
+    result = load_addresses()
+    for f in filters:
+        result = filter(f, result)
+    return list(result)
+
+
+def make_address_string(location_dict):
+    street = location_dict['adressenavn']
+    number = location_dict['husnr']
+    letter = location_dict.get('bokstav', '')
+
+    return "{} {} {}".format(street, number, letter)
+
+
+def fetch_guid(address, api_location='https://trashtalkapi.azurewebsites.net/api/trashcan/create'):
+    body = {
+          "lat": address['nord'],
+          "long": address['aust'],
+          "address": make_address_string(address)
+        }
+
+    return requests.post(api_location, data=body).json()
+
+
+def post_data_for_one_guid(guid, data, api_prefix = 'https://trashtalkapi.azurewebsites.net/api/trashcan/'):
+    post_url = api_prefix + guid + '/status'
+    post_headers = {'Content-Type': 'application/json'}
+    for sensor_data in data:
+        requests.post(url=post_url, headers=post_headers, data=sensor_data)
+    print("Posted for: {} \ndata: {}".format(guid, data))
+
+
+def post_dummy_data(data_map):
+    for guid, data in data_map.items():
+        post_data_for_one_guid(guid, data)
+
+
+def generate_and_post_dummy_data():
+    addresses = get_subset_of_addresses()
+
+    guids = [fetch_guid(address) for address in addresses]
+
+    data = generate_n_per_guid(guids)
+
+    post_dummy_data(data)
+
+
 if __name__ == '__main__':
-    print(generate_n_per_guid(('a','b')))
+    generate_and_post_dummy_data()
